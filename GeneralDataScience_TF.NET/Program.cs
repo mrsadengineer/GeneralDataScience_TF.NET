50971e1b1c4f01bedba316a090f31f97475c1b45
Sam <dsaeresearch@gmail.com>
2020-11-25 09:52:58 -0500

    Add project files.

---
+++ GeneralDataScience_TF.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeneralDataScience_TF.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
